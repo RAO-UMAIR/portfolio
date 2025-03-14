ab4c6d128675139501e045d1bd9420156f943687
RAO UMAIR <ru1063936@gmail.com>
2025-03-14 12:25:57 -0700

    Changed the path of frontend in  production

--- backend/server.js
+++ backend/server.js
@@ -14,8 +14,10 @@ const port = process.env.PORT || 5000;
 
 connectDB();
 
+const __dirname = path.resolve();
+
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "frontend", "build")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
@@ -28,4 +30,4 @@ if (process.env.NODE_ENV === "production") {
 
 app.use("/api/contacts", contactRouter);
 
-app.listen(port, () => console.log("server is running on port 5000"));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
